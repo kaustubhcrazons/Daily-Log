eabb5364a270a96796fa1667f5cd77b1b42f1c76
smuggl3r <kunalpratap750@gmail.com>
2026-05-19 15:33:16 +0530

    sheet update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 // 🔗 YOUR APPS SCRIPT URL
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzgfwP1ntwpMJTP8AA1oV1Sxk1FyKKJEEc772mZGd_4eHXVvsOXg65Jx9D_kCpEdgxmyQ/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxKHWPnmCFxc7P_Tdfwmdx24ba4ID9K5OzgRQYP6FoWZ5iB6TL4yz2a3CBL085DAzOZBw/exec";
 
 // ================= LOGIN =================
 const users = JSON.parse(fs.readFileSync('users.json'));
