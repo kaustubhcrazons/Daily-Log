553312aa1afd8349be8387bf1769f2d9f3656412
kaustubhcrazons <kaustubh@crazons.com>
2026-09-08 12:30:04 +0530

    newdash

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 // 🔗 YOUR APPS SCRIPT URL
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1veQ8eSl_g8OdkGTdlNiwgbTzUhDJ5S1VZuF9Lo6zurG8j3QKZfIVh8NC_guF6y4uhg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz1veQ8eSl_g8OdkGTdlNiwgbTzUhDJ5S1VZuF9Lo6zurG8j3QKZfIVh8NC_guF6y4uhg/execg";
 
 // ================= LOGIN =================
 const users = JSON.parse(fs.readFileSync('users.json'));
